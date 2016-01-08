6971710b9feeb33c54e418310487dcc1f62adb67
FranciscoG <frankjg@gmail.com>
2016-01-08 10:45:35 -0500

    removed local jquery and updated polling function

--- Chrome/script.js
+++ Chrome/script.js
@@ -1,29 +1,36 @@
 (function(){
+  'use strict';
 
   // if you're not 100% sure if jquery exists, load it if it's missing
-  var timeout = 100; // 10 seconds timeout
-  var poll = function (cb) {
-    setTimeout(function () {
-      timeout--;
-      if (typeof window.jQuery !== 'undefined') {
-        cb(); // run callback
-      } else if (timeout > 0) {
-        poll(); // do it again sam
-      } else {
-        // External library failed to load, handle error
-        console.log('extension:  jquery did not load in time');
-      }
-    }, 100);
+  var poll = function(waitingFor, cb) {
+    var interval = 100; // ms
+    var currInterval = 0;
+    var limit = 50; // how many intervals
+
+    var check = function () {
+        if (typeof waitingFor !== 'undefined') {
+            console.log(waitingFor, "is available");
+            cb(); // we'll assume typeof cb is a function for now
+        } else if (currInterval < limit) {
+            currInterval++;
+            console.log('waiting for', waitingFor);
+            window.setTimeout(check, interval);
+        } else {
+          console.log(waitingFor, 'did not load in the last', limit * interval, 'ms');
+        }
+    };
+
+    window.setTimeout(check, interval);
   };
 
   function loadjQuery(callback) {
     // let's load jQuery if it doesn't exist
     if (typeof window.jQuery === 'undefined') {
       var s = document.createElement('script');
-      s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
+      s.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
       s.async = true;
       document.head.appendChild(s);
-      poll(callback);
+      poll(window.jQuery, callback);
     } else {
       callback();
     }
