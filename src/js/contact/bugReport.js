ab104f2ad8ea07010a79845d87549f0d618928af
franciscog <frankjg@gmail.com>
2016-06-21 00:05:12 -0400

    switching to browserify

--- src/js/contact/bugReport.js
+++ src/js/contact/bugReport.js
@@ -1,4 +1,6 @@
-dubx.report_content = function() {
+var modal = require('../utils/modal.js');
+
+var report_content = function() {
     var content = $('.input').val();
 
     if(!content || content.trim(' ').length === 0) {return;}
@@ -21,8 +23,13 @@ dubx.report_content = function() {
     $('.report').replaceWith('<li onclick="" class="for_content_li for_content_feature report"><p class="for_content_off"><i class="fi-check"></i></p><p class="for_content_p">Bug Report</p></li>');
 };
 
-dubx.report_modal = function() {
-    dubx.input('Bug Report:','','Please only report bugs for DubX, not Dubtrack. \nBe sure to give a detailed description of the bug, and a way to replicate it, if possible.','confirm-for36','999');
-    $('.confirm-for36').click(dubx.report_content);
-    $('.confirm-for36').click(dubx.closeErr);
+var report_modal = function() {
+    modal.input('Bug Report:','','Please only report bugs for DubX, not Dubtrack. \nBe sure to give a detailed description of the bug, and a way to replicate it, if possible.','confirm-for36','999');
+    $('.confirm-for36').click(report_content);
+    $('.confirm-for36').click(modal.closeErr);
+};
+
+module.exports = {
+    report_content: report_content,
+    report_modal: report_modal
 };
